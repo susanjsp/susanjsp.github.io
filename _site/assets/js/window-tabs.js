237f72cb4dc9ae7268667b9687b919ebb4178016
Susan Pan <susan.pan96@gmail.com>
2021-01-17 17:22:03 +1000

    updated window-tabs js

--- _site/assets/js/window-tabs.js
+++ _site/assets/js/window-tabs.js
@@ -1,16 +1,24 @@
 function initWindowTabs() {
+  const allTabs = document.querySelectorAll('.header a');
+  allTabs.forEach(tab => tab.classList.remove('header-active'));
+
   switch(window.location.pathname) {
     case "/":
-      const homeActive = document.querySelector('#home-link');
+      const homeActive = document.querySelector('a[data-tab="home"]');
       homeActive.classList.add('header-active');
       break;
-    case "/projects":
-      const projectActive = document.querySelector('#projects-link');
-      projectActive.classList.add('header-active');
+    case "/about":
+      const aboutActive = document.querySelector('a[data-tab="about"]');
+      aboutActive.classList.add('header-active');
       break;
     case "/contact":
-      const contactActive = document.querySelector('#contact-link');
+      const contactActive = document.querySelector('a[data-tab="contact"]');
       contactActive.classList.add('header-active');
+      break;
+    default:
+      const projectActive = document.querySelector('a[data-tab="projects"]');
+      projectActive.classList.add('header-active');
+      break;
   }
 }
 
